d359486d5db6d8ce5a31fd5801f2290ba14e1228
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-19 16:18:23 +0100

    first get endpoint
    first get endpoint

---
+++ backend-springboot/src/main/java/com/reactive/app/springboot/backend/backendspringboot/controller/ReservationResource.java
@@ -0,0 +1,26 @@
+package com.reactive.app.springboot.backend.backendspringboot.controller;
+
+import com.reactive.app.springboot.backend.backendspringboot.model.Reservation;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping(ReservationResource.ROOM_V_1_RESERVATION)
+@CrossOrigin
+public class ReservationResource {
+
+    public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation/";
+
+
+    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationId(@PathVariable String roomId) {
+        //reservationService.getReservation(roomId);
+
+        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now(), 45 , "ID56POV");
+        return Mono.just(reservation);
+
+    }
+}
