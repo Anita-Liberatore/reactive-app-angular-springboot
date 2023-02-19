fe9586344a0a20b2ea9c3cbad8c11d538ea4df08
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-19 15:50:22 +0100

    start project

---
+++ backend-springboot/src/main/java/com/reactive/app/springboot/backend/backendspringboot/BackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.app.springboot.backend.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/reactive/app/springboot/backend/backendspringboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.app.springboot.backend.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
